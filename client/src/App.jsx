7005fe1012a23d7aa29abcbec672509ee1231d11
ankeshkumarsingh2791 <ankeshkumarsingh2791@gmail.com>
2025-08-06 22:29:49 +0530

    Project  setUp

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full flex justify-center items-center h-screen bg-green-400 text-amber-100 text-3xl'><h1>
+      Hello welcome to Qurvit!!!!!!!!!!!!
+      </h1></div>
+  )
+}
+
+export default App
